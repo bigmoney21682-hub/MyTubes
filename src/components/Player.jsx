f0d895256e20f85e1b85cdaa2c2ed5d36ef9e568
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-19 21:38:03 -0500

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,50 +1,20 @@
 // File: src/components/Player.jsx
-
-import React, { forwardRef, useEffect } from "react";
+import { forwardRef } from "react";
 import ReactPlayer from "react-player";
 
-const Player = forwardRef(({ src, playing, title, author }, ref) => {
-  useEffect(() => {
-    if (!src) return;
-    if ("mediaSession" in navigator) {
-      navigator.mediaSession.metadata = new MediaMetadata({
-        title: title || "Playing",
-        artist: author || "Unknown",
-        artwork: [
-          { src: "/favicon.png", sizes: "512x512", type: "image/png" }
-        ]
-      });
-
-      navigator.mediaSession.setActionHandler("play", () => {});
-      navigator.mediaSession.setActionHandler("pause", () => {});
-      navigator.mediaSession.setActionHandler("previoustrack", () => {});
-      navigator.mediaSession.setActionHandler("nexttrack", () => {});
-    }
-  }, [src, title, author]);
-
+const Player = forwardRef(({ src, playing, onEnded }, ref) => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        width: "100%",
-        height: 60,
-        background: "#000",
-        zIndex: 9999
-      }}
-    >
-      <ReactPlayer
-        ref={ref}
-        url={src}
-        playing={playing}
-        controls
-        width="100%"
-        height="100%"
-        style={{ position: "absolute", top: 0, left: 0 }}
-        playsinline
-      />
-    </div>
+    <ReactPlayer
+      ref={ref}
+      url={src}
+      width="0px"
+      height="0px"
+      playing={playing}
+      onEnded={onEnded}
+      controls={false}
+      volume={1}
+      muted={false}
+    />
   );
 });
 
