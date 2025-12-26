e60cf762572f24edd1cd61795653f251ad9482c5
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-25 21:55:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Watch from "./pages/Watch";
-import Search from "./pages/Search";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/watch" element={<Watch />} />
-      <Route path="/search" element={<Search />} />
 
-      {/* Catch-all fallback */}
+      {/* fallback */}
       <Route path="*" element={<Home />} />
     </Routes>
   );
