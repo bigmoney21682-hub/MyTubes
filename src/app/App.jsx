1d6ffdf9bbb3353f9dc7b9f25c08b5196fd3b203
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-26 12:29:11 -0500

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,18 @@
+/**
+ * File: App.jsx
+ * Path: src/app/App.jsx
+ * Description: Root application component. Mounts routing and global debug overlay.
+ */
+
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
 import DebugOverlay from "../debug/DebugOverlay";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Routes />
       <DebugOverlay />
-    </>
+    </BrowserRouter>
   );
 }
