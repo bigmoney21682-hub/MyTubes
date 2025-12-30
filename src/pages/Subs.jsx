aed07e2e26c4d5ffc0a41e3802fe46d2bafd7d35
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-30 08:36:02 -0500

    Create Subs.jsx

---
+++ src/pages/Subs.jsx
@@ -0,0 +1,16 @@
+/**
+ * File: Subs.jsx
+ * Path: src/pages/Subs.jsx
+ * Description: Placeholder Subscriptions page.
+ */
+
+import React from "react";
+
+export default function Subs() {
+  return (
+    <div style={{ padding: 16, color: "#fff" }}>
+      <h2>Subscriptions</h2>
+      <p>Your subscribed channels will appear here.</p>
+    </div>
+  );
+}
